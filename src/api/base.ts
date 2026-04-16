fd6b6822cb249a6df62e7eed2141233deac73c7e
vamsi-intern-synclovis <vamsi.intern.synclovis@gmail.com>
2026-04-16 16:36:09 +0530

    initial commit again

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:5000/api/";
-// const baseURL = "https://your-production-url/api/";
+// const baseURL = "http://localhost:5000/api/";
+const baseURL = "https://task-tracker-backend-lyu3.onrender.com/api/";
 
 export default baseURL;
